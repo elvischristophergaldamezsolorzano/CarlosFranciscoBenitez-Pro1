699a73f3449dd5d3c98ceb31743610f4f86ed35d
CarlosBenitez <bq0006032020@unab.edu.sv>
2021-02-10 13:29:55 -0600

    Login

---
+++ CarlosB01/CarlosB01/NEGOCIO/ClsLogin.cs
@@ -0,0 +1,14 @@
+﻿using CarlosB01.DOMINIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarlosB01.NEGOCIO
+{
+    class ClsLogin
+    {
+        Login log = new Login();
+    }
+}
